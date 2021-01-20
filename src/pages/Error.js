26502a7e31d1a3349c6072ace8cb324329a79fb6
frankinstyyn <frankie-cormack@hotmail.com>
2021-01-20 16:48:01 +0000

    Updated to clone template site

---
+++ src/pages/Error.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Hero from "../components/Hero"
+import Banner from "../components/Banner";
+import { Link } from "react-router-dom";
+
+export default function Home() {
+  return (
+    <Hero hero="defaultHero" >
+      <Banner title="404 Error" subtitle="This page does not exist">
+        <Link to="/" className="btn-primary">
+          Back to Home
+      </Link>
+      </Banner>
+    </Hero>
+  );
+};
+
