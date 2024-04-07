10220562d7ff853f883bb12a96b0202b89d1201b
Frankinstyyn <49882948+frankinstyyn@users.noreply.github.com>
2024-04-07 16:04:28 +0100

    Update contact form

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,18 +1,32 @@
-import React from 'react';
+// Inside your React component
+import React, { useEffect } from 'react';
 
-const ContactForm = () => {
+function ContactForm() {
+  useEffect(() => {
+    window.addEventListener('message', handleMessage);
+    return () => {
+      window.removeEventListener('message', handleMessage);
+    };
+  }, []);
+
+  const handleMessage = (event) => {
+    if (event.data === 'messageSent') {
+      // Hide the iframe and show the thank you message div
+      const iframe = document.querySelector('iframe');
+      const thankYouMessage = document.getElementById('popUp');
+      iframe.style.display = 'none';
+      thankYouMessage.style.display = 'block';
+    }
+  };
+
   return (
-    <div className='bannerForm'>
-     <h3 className="headerRed">Get in touch for a free no obligation quotation</h3>
-     <div className='iFrameContainer'>
-      <iframe
-        title="Contact Form"
-        src="https://form.jotform.com/240945760649063"
-        width="100%"
-        height="600"
-        frameBorder="0"
-        scrolling="yes"
-        ></iframe>
+    <div className="bannerForm">
+      <h3 className="headerRed">Get in touch for a free no obligation quotation</h3>
+      <iframe style={{ border: 'none' }} src="https://sparkly-fudge-eba2c9.netlify.app/" title="Contact Form" />
+      <div id="popUp" class="msg">
+        <p>
+        Hi there. Many thanks for your interest in Weedpro. Someone will respond within the next 24 hours!
+        </p>
     </div>
     </div>
   );
